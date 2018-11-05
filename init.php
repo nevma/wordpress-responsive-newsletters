764903dcebf309e0da724bdc37135fbb43b02f4f
Takis Bouyouris <takis@nevma.gr>
2018-11-05 16:20:48 +0200

    Meddle with TinyMCE only on backend edit screens.

--- init.php
+++ init.php
@@ -18,8 +18,12 @@
 
 
     // << Takis
-    add_filter( 'mce_css', 'mnltr_cpt_add_editor_styles', 999 );
-    add_action( 'admin_print_footer_scripts', 'mnltr_cpt_tinymce_autogrow', 999 );
+    if ( is_admin() ) {
+
+        add_filter( 'mce_css', 'mnltr_cpt_add_editor_styles', 999 );
+        add_action( 'admin_print_footer_scripts', 'mnltr_cpt_tinymce_autogrow', 999 );
+        
+    }
     // >> Takis
     
 
